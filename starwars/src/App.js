9366882dd6a43d02cf36dbeddcdbf7bb92b6aa74
Phil Lisak <phil.lisak@gmail.com>
2020-07-31 18:35:54 -0700

    API Call completed, now I can start creating Cards

--- starwars/src/App.js
+++ starwars/src/App.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import Character from './components/Character'
 import './App.css';
 
 const App = () => {
@@ -11,9 +12,9 @@ const App = () => {
 
   return (
     <div className="App">
-      <h1 className="Header">Characters</h1>
+      <h1 className="Header"><Character /></h1>
     </div>
-  );
+  )
 }
 
 export default App;
--- starwars/src/components/Character.js
+++ starwars/src/components/Character.js
@@ -1,1 +1,27 @@
-// Write your Character component here
+
+import React, { useState, useEffect } from 'react';
+import axios from 'axios';
+
+export default function Character() {
+const [charData, setCharData] = useState([]);
+
+useEffect(() => {
+    axios.get('https://rickandmortyapi.com/api/character/')
+    .then((response) => {
+        console.log(response)
+        // setCharData(response.results)
+    })
+    .catch((error) => {
+        console.log(error)
+    })
+}, [])
+
+
+
+
+
+    return (
+<div></div>
+    )
+}
+
