d8fdeba9f0acf57d073af6b9d774a3867c5bfc71
Phil Lisak <phil.lisak@gmail.com>
2020-07-31 22:41:27 -0700

    Didn't break the thing. Removed unnecessary component. Grid styled. Next step is figuring out how to go to next page

--- starwars/src/App.js
+++ starwars/src/App.js
@@ -1,5 +1,6 @@
-import React from 'react';
-import Character from './components/Character'
+import React, { useState, useEffect } from 'react';
+import CharCard from './components/CharCard';
+import axios from 'axios';
 import styled from 'styled-components';
 import './App.css';
 
@@ -16,18 +17,33 @@ color: white;
 box-shadow: 0px 1px 6px -2px grey;
 `
 
+const Start = styled.div`
+    display: flex;
+    justify-content: space-between;
+    flex-wrap: wrap;
+`
+
 const App = () => {
-  // Try to think through what state you'll need for this app before starting. Then build out
-  // the state properties here.
+  const [charData, setCharData] = useState([]);
 
-  // Fetch characters from the API in an effect hook. Remember, anytime you have a 
-  // side effect in a component, you want to think about which state and/or props it should
-  // sync up with, if any.
-
+  useEffect(() => {
+      axios.get('https://rickandmortyapi.com/api/character/')
+      .then((response) => {
+          console.log(response)
+          setCharData(response.data.results)
+      })
+      .catch((error) => {
+          console.log(error)
+      })
+  }, [])
+  
   return (
     <div className="App">
       <Container>
-      <h1 className="Header"><Character /></h1>
+      <h1 className="Header">L-look at All the -urp, All the Characters Morty! Alllll the Characters...</h1>
+          <Start className='start'>
+              {charData.map(item => <CharCard key={item.id} name={item.name} image={item.image} species={item.species} location={item.location} status={item.status} />)}
+          </Start>      
       </Container>
     </div>
   )
