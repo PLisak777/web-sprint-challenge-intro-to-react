e96a0161a9b32ec588a3d352c8056f61bc13f095
Phil Lisak <phil.lisak@gmail.com>
2020-07-31 20:38:56 -0700

    I DO NOT understand this module for some reason. Not done.

---
+++ starwars/src/components/CharCard.js
@@ -0,0 +1,19 @@
+import React, { useState } from 'react';
+import Character from './Character';
+import styled from 'styled-components';
+
+export default function CharCard() {
+// const [charData, setCharData] = props;
+
+const CardHead = styled.div`
+display: flex;
+flex-direction: column;
+height: 150px;
+width: 100%;
+flex: 1;
+`
+
+    return (
+    <CardHead>Name: </CardHead>
+    )
+};
--- starwars/src/components/Character.js
+++ starwars/src/components/Character.js
@@ -1,27 +1,29 @@
 
 import React, { useState, useEffect } from 'react';
 import axios from 'axios';
+import CharCard from './CharCard'
 
 export default function Character() {
 const [charData, setCharData] = useState([]);
+
 
 useEffect(() => {
     axios.get('https://rickandmortyapi.com/api/character/')
     .then((response) => {
         console.log(response)
-        // setCharData(response.results)
+        setCharData(response.results)
     })
     .catch((error) => {
         console.log(error)
     })
 }, [])
 
-
-
-
+console.log(charData)
 
     return (
-<div></div>
+        <div>
+            {/* {charData.map(data => <CharCard key={data.id} name={data.name} />)} */}
+        </div>
     )
 }
 
