1bab5d3c6f2f43dea6b8e418ac1d85316ddd57e3
Phil Lisak <phil.lisak@gmail.com>
2020-07-31 22:33:32 -0700

    About to break my thing moving code around

--- starwars/src/components/CharCard.js
+++ starwars/src/components/CharCard.js
@@ -1,11 +1,10 @@
 import React from 'react';
-import Character from './Character';
 import styled from 'styled-components';
 
 export default function CharCard(props) {
 
 const Card = styled.div`
-    background: #99f3eb;
+    background: #c1f762;
     color: black;
     width: 200px;
     max-height: 300px;
@@ -13,7 +12,7 @@ const Card = styled.div`
     flex-direction: column;
     align-items: center;
     justify-content: space-between;
-    margin-bottom: 20px;
+    margin: 20px;
     cursor: pointer;
     box-shadow: 0px 1px 6px -2px grey;
 `
@@ -21,7 +20,8 @@ const Card = styled.div`
 const CardHead = styled.h3`
     margin: 0;
     font-size: 1rem;
-    background: lightgrey;
+    font-weight: 300;
+    background: #c1f762;
 `
 
 const CardImg = styled.div`
@@ -32,17 +32,18 @@ const CardImg = styled.div`
 `
 
 const CardContent = styled.div`
-    font-size: 1rem;
-    line-height: 1;
-    background: lightgrey;
+    font-size: .75rem;
+    line-height: .5;
+    background: #c1f762;
+    font-weight: 200;
 `
 
     return (
         <Card>
-            <CardHead>Name: {props.name} </CardHead>
+            <CardHead>Designation: {props.name} </CardHead>
             <CardImg><img className='card-img' src={props.image} alt='' /></CardImg>
             <CardContent>
-                <p>Species: {props.species}</p>
+                {/* <p>Species: {props.species}</p> */}
                 <p>Location: {props.location.name}</p>
                 <p>Status: {props.status}</p>
             </CardContent>
